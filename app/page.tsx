f691f3609da936ef03e58d4d1fd99d11837338a3
Michael Vittiglio <michael.vittiglio@gmail.com>
2023-04-25 14:48:12 -0400

    127 Search page

--- app/page.tsx
+++ app/page.tsx
@@ -26,50 +26,84 @@ export default function Home() {
           </nav>
           {/* NAVBAR */}
           {/* HEADER */}
-          <div className="h-96 overflow-hidden">
-            <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
-              <h1 className="text-7xl text-white capitalize text-center text-shadow">
-                Mike's Place (Little Falls)
-              </h1>
+          <div className="bg-gradient-to-r to-[#5f6984] from-[#0f1f47] p-2">
+            {/* SEARCH BAR */}
+            <div className="text-left text-lg py-3 m-auto flex justify-center">
+              <input
+                className="rounded mr-3 p-2 w-[450px] bg-white"
+                type="text"
+                placeholder="State, city, or town"
+              />
+              <button className="rounded bg-red-600 px-9 py-2 text-white">
+                Let's go
+              </button>
             </div>
+            {/* SEARCH BAR */}
           </div>
           {/* HEADER */}
-          {/* BODY */}
-          <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-            {/* DESCRIPTION PORTION */}
-            <div className="bg-white w-[100%] rounded p-3 shadow">
-              {/* RESTAURANT NAVBAR */}
-              <nav className="flex text-reg border-b pb-2">
-                <a href="" className="mr-7">
-                  Overview
-                </a>
-                <a href="" className="mr-7">
-                  Menu
-                </a>
-              </nav>
-              {/* RESTAURANT NAVBAR */}
-              <main className="bg-white mt-10">
-                <div>
-                  <div className="mt-4 pb-1 mb-1">
-                    <h1 className="font-bold text-4xl">Menu</h1>
+          <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+            {/* SEARCH SIDE BAR */}
+            <div className="w-1/5">
+              <div className="border-b pb-4">
+                <h1 className="mb-2">Regions</h1>
+                <p className="font-light text-reg">Toronto</p>
+                <p className="font-light text-reg">Ottawa</p>
+                <p className="font-light text-reg">Montreal</p>
+                <p className="font-light text-reg">Hamilton</p>
+                <p className="font-light text-reg">Kingston</p>
+                <p className="font-light text-reg">Niagra</p>
+              </div>
+              <div className="border-b pb-4 mt-3">
+                <h1 className="mb-2">Cuisines</h1>
+                <p className="font-light text-reg">Mexican</p>
+                <p className="font-light text-reg">Italian</p>
+                <p className="font-light text-reg">Chinese</p>
+              </div>
+              <div className="mt-3 pb-4">
+                <div className="mb2">Price</div>
+                <div className="flex">
+                  <button className="border-t border-b border-l w-full text-reg font-light rounded-l p-2">
+                    $
+                  </button>
+                  <button className="border w-full text-reg font-light p-2">
+                    $$
+                  </button>
+                  <button className="border-t border-b border-r w-full text-reg font-light rounded-r p-2">
+                    $$$
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* SEARCH SIDE BAR */}
+            {/* RESTAURANT CARD */}
+            <div className="w-5/6">
+              <div className="border-b flex pb-5">
+                <img
+                  src="https://resizer.otstatic.com/v2/photos/xlarge/1/24841578.jpg"
+                  alt=""
+                  className="w-44 rounded"
+                />
+                <div className="pl-5">
+                  <h2 className="text-3xl">Mike's Place</h2>
+                  <div className="flex items-start">
+                    <div className="flex mb-2">*****</div>
+                    <p className="ml-2 text-sm">Awesome</p>
                   </div>
-                  <div className="flex flex-wrap justify-between">
-                    {/* MENU CARD */}
-                    <div className="border rounded p-3 w-[49%] mb-3">
-                      <h3 className="font-bold text-lg">Surf & Turf</h3>
-                      <p className="font-light mt-1 text-sm">
-                        A medium steak with lobster and medium rice.
-                      </p>
-                      <p className="mt-7">$80.00</p>
+                  <div className="mb-9">
+                    <div className="font-light flex text-reg">
+                      <p className="mr-4">$$$</p>
+                      <p className="mr-4">Mexican</p>
+                      <p className="mr-4">Ottawa</p>
                     </div>
-                    {/* MENU CARD */}
+                  </div>
+                  <div className="text-red-600">
+                    <a href="">View more information</a>
                   </div>
                 </div>
-              </main>
+              </div>
             </div>
-            {/* DESCRIPTION PORTION */}
+            {/* RESTAURANT CARD */}
           </div>
-          {/* BODY */}
         </main>
       </main>
     </main>
