937d7d707667aa36a9ac50ffb1ee29fe0773ba8c
Michael Vittiglio <michael.vittiglio@gmail.com>
2023-04-25 12:06:10 -0400

    124 The image gallery

--- app/page.tsx
+++ app/page.tsx
@@ -80,6 +80,86 @@ export default function Home() {
                 </p>
               </div>
               {/* DESCRIPTION */}
+              {/* IMAGES */}
+              <div>
+                <h1 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">
+                  7 photos
+                </h1>
+                <div className="flex flex-wrap">
+                  <img
+                    className="w-56 h-44 mr-1 mb-1"
+                    src="https://resizer.otstatic.com/v2/photos/xlarge/1/26051596.jpg"
+                    alt=""
+                  />
+                  <img
+                    className="w-56 h-44 mr-1 mb-1"
+                    src="https://resizer.otstatic.com/v2/photos/xlarge/1/26051597.jpg"
+                    alt=""
+                  />
+                  <img
+                    className="w-56 h-44 mr-1 mb-1"
+                    src="https://resizer.otstatic.com/v2/photos/xlarge/1/26051598.jpg"
+                    alt=""
+                  />
+                  <img
+                    className="w-56 h-44 mr-1 mb-1"
+                    src="https://resizer.otstatic.com/v2/photos/xlarge/1/26051600.jpg"
+                    alt=""
+                  />
+                  <img
+                    className="w-56 h-44 mr-1 mb-1"
+                    src="https://resizer.otstatic.com/v2/photos/xlarge/1/26051604.jpg"
+                    alt=""
+                  />
+                  <img
+                    className="w-56 h-44 mr-1 mb-1"
+                    src="https://resizer.otstatic.com/v2/photos/xlarge/1/26051605.jpg"
+                    alt=""
+                  />
+                  <img
+                    className="w-56 h-44 mr-1 mb-1"
+                    src="https://resizer.otstatic.com/v2/photos/xlarge/1/26051606.jpg"
+                    alt=""
+                  />
+                  <img className="w-56 h-44 mr-1 mb-1" src="" alt="" />
+                </div>
+              </div>
+              {/* IMAGES */}
+              {/* REVIEWS */}
+              <div>
+                <h1 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">
+                  What 100 people are saying
+                </h1>
+                <div>
+                  {/* REVIEW CARD */}
+                  <div className="border-b pb-7 mb-7">
+                    <div className="flex">
+                      <div className="w-1/6 flex flex-col items-center">
+                        <div className="rounded-full bg-blue-400 w-16 h-16 flex items-center justify-center">
+                          <h2 className="text-white text-2xl">MJ</h2>
+                        </div>
+                        <p className="text-center">Michael Jordan</p>
+                      </div>
+                      <div className="ml-10 w-5/6">
+                        <div className="flex items-center">
+                          <div className="flex mr-5">*****</div>
+                        </div>
+                        <div className="mt-5">
+                          <p className="text-lg font-light">
+                            The call ahead seating feature offered by open table
+                            worked well. Food is good but not great, best part
+                            of the evening was Laura, a very upbeat and friendly
+                            server who was genuinely caring and helpful to all
+                            of us.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  {/* REVIEW CARD */}
+                </div>
+              </div>
+              {/* REVIEWS */}
             </div>
           </div>
           {/* DESCRIPTION PORTION */}
