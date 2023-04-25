0fcaaa46285df6e240eebd2bd529ceb91877ad6e
Michael Vittiglio <michael.vittiglio@gmail.com>
2023-04-25 11:31:27 -0400

    122 The restaurant page

--- app/page.tsx
+++ app/page.tsx
@@ -5,5 +5,37 @@ import styles from "./page.module.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export default function Home() {
-  return <main></main>
+  return (
+    <main>
+      <main className="bg-gray-100 min-h-screen w-screen">
+        <main className="max-w-screen-2xl m-auto bg-white text-black">
+          {/* NAVBAR */}
+          <nav className="bg-white p-2 flex justify-between">
+            <a href="" className="font-bold text-gray-700 text-2xl">
+              {" "}
+              OpenTable{" "}
+            </a>
+            <div>
+              <div className="flex">
+                <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
+                  Sign In
+                </button>
+                <button className="border p-1 px-4 rounded">Sign Up</button>
+              </div>
+            </div>
+          </nav>
+          {/* NAVBAR */}
+          {/* HEADER */}
+          <div className="h-96 overflow-hidden">
+            <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
+              <h1 className="text-7xl text-white capitalize text-center text-shadow">
+                Mike's Place (Little Falls)
+              </h1>
+            </div>
+          </div>
+          {/* HEADER */}
+        </main>
+      </main>
+    </main>
+  )
 }
