5e1ce428547c57118277d2a5b0ea5c948c788d9c
Michael Vittiglio <michael.vittiglio@gmail.com>
2023-04-25 12:45:55 -0400

    125 The reservation card

--- app/page.tsx
+++ app/page.tsx
@@ -34,8 +34,9 @@ export default function Home() {
             </div>
           </div>
           {/* HEADER */}
-          {/* DESCRIPTION PORTION */}
+          {/* BODY */}
           <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+            {/* DESCRIPTION PORTION */}
             <div className="bg-white w-[70%] rounded p-3 shadow">
               {/* RESTAURANT NAVBAR */}
               <nav className="flex text-reg border-b pb-2">
@@ -161,8 +162,54 @@ export default function Home() {
               </div>
               {/* REVIEWS */}
             </div>
+            {/* DESCRIPTION PORTION */}
+            {/* RESERVATION CARD PORTION */}
+            <div className="w-[27%] relative text-reg">
+              <div className="fixed w-[15%] bg-white rounded p-3 shadow">
+                <div className="text-center border-b pb-2 font-bold">
+                  <h4 className="mr-7 text-lg">Make a Reservation</h4>
+                </div>
+                <div className="my-3 flex flex-col">
+                  <label htmlFor="">Party size</label>
+                  <select
+                    name=""
+                    className="bg-white py-3 border-b font-light"
+                    id=""
+                  >
+                    <option value="">1 person</option>
+                    <option value="">2 people</option>
+                  </select>
+                </div>
+                <div className="flex justify-between">
+                  <div className="flex flex-col w-[48%]">
+                    <label>Date</label>
+                    <input
+                      type="text"
+                      className="bg-white py-3 border-b font-light w-28"
+                    />
+                  </div>
+                  <div className="flex flex-col w-[48%]">
+                    <label>Time</label>
+                    <select
+                      name=""
+                      id=""
+                      className="bg-white py-3 border-b font-light"
+                    >
+                      <option value="">7:30 AM</option>
+                      <option value="">12:30 7PM</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="mt-5">
+                  <button className="bg-red-500 rounded w-full px-4 text-white font-bold h-16">
+                    Find a Time
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* RESERVATION CARD PORTION */}
           </div>
-          {/* DESCRIPTION PORTION */}
+          {/* BODY */}
         </main>
       </main>
     </main>
