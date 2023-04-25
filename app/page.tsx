7432182b5eb68f0bc4eb3238dac25458d73fced7
Michael Vittiglio <michael.vittiglio@gmail.com>
2023-04-25 11:45:38 -0400

    123 The title and description

--- app/page.tsx
+++ app/page.tsx
@@ -34,6 +34,55 @@ export default function Home() {
             </div>
           </div>
           {/* HEADER */}
+          {/* DESCRIPTION PORTION */}
+          <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
+            <div className="bg-white w-[70%] rounded p-3 shadow">
+              {/* RESTAURANT NAVBAR */}
+              <nav className="flex text-reg border-b pb-2">
+                <a href="" className="mr-7">
+                  Overview
+                </a>
+                <a href="" className="mr-7">
+                  Menu
+                </a>
+              </nav>
+              {/* RESTAURANT NAVBAR */}
+              {/* TITLE */}
+              <div className="mt-4 border-b pb-6">
+                <h1 className="font-bold text-6xl">Mike's Place</h1>
+              </div>
+              {/* TITLE */}
+              {/* RATINGS */}
+              <div className="flex items-end">
+                <div className="ratings mt-2 flex items-center">
+                  <p>*****</p>
+                  <p className="text-reg ml-3">4.9</p>
+                </div>
+                <div className="text-reg ml-4">420 Reviews</div>
+              </div>
+              {/* RATINGS */}
+              {/* DESCRIPTION */}
+              <div className="mt-4">
+                <p className="text-lg font-light">
+                  Red Lobster welcomes you by offering PRIORITY SEATING to
+                  reduce your wait time in the restaurant, so you can spend more
+                  time doing the things you love. Or, come enjoy a drink at our
+                  bar and a Tasting Plate while you wait. Red Lobster is the
+                  world's largest and most loved seafood restaurant company,
+                  offering high quality, freshly-prepared seafood, sourced in a
+                  way that is traceable, sustainable and responsible. We offer
+                  lunch and dinner daily, featuring classics like our Ultimate
+                  Feast®, Garlic Shrimp Scampi and Parrot Isle Jumbo Coconut
+                  Shrimp, and feature limited time flavors and preparations
+                  during events like Lobsterfest®, Crabfest® and Endless
+                  Shrimp®. And, who can resist our famous warm and cheesy
+                  Cheddar Bay Biscuits®?
+                </p>
+              </div>
+              {/* DESCRIPTION */}
+            </div>
+          </div>
+          {/* DESCRIPTION PORTION */}
         </main>
       </main>
     </main>
