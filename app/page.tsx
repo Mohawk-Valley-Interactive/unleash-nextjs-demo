4bd8b396b151368951b355e18c2aa1e009e15885
Michael Vittiglio <michael.vittiglio@gmail.com>
2023-04-27 16:47:34 -0400

    14 Programmatic navigation using useRouter hook.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+'use client';
+
 import Link from 'next/link';
 import { Inter } from '@next/font/google';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const router = useRouter();
+  const [location, setLocation] = useState('');
+
   return (
     <div className="bg-gray-100 min-h-screen w-screen">
       <div className="max-w-screen-2xl m-auto bg-white text-black">
@@ -38,8 +45,18 @@ export default function Home() {
                   className="rounded mr-3 p-2 w-[450px] bg-white"
                   type="text"
                   placeholder="State, city, or town"
+                  value={location}
+                  onChange={(e) => setLocation(e.target.value)}
                 />
-                <button className="rounded bg-red-600 px-9 py-2 text-white">Let's go</button>
+                <button
+                  className="rounded bg-red-600 px-9 py-2 text-white"
+                  onClick={() => {
+                    if (location === 'banana') return;
+                    router.push('/search');
+                  }}
+                >
+                  Let's go
+                </button>
               </div>
               {/* SEARCH BAR */}
             </div>
