776e150687b076059a25cb0acc02181ad0a6b353
Michael Vittiglio <michael.vittiglio@gmail.com>
2023-04-28 15:57:34 -0400

    21 Placing universally used components in the root layout.

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,19 +1,12 @@
-import Link from 'next/link';
-import NavBar from '../../components/NavBar';
-import Header from './components/Header';
-import Form from './components/Form';
+import Header from "./components/Header";
+import Form from "./components/Form";
 
 export default function Reserve() {
   return (
-    <div className="bg-gray-100 min-h-screen w-screen">
-      <div className="max-w-screen-2xl m-auto bg-white text-black">
-        <NavBar />
-        <div className="border-t h-screen">
-          <div className="w-3/5 py-9 m-auto">
-            <Header />
-            <Form />
-          </div>
-        </div>
+    <div className="border-t h-screen">
+      <div className="w-3/5 py-9 m-auto">
+        <Header />
+        <Form />
       </div>
     </div>
   );
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,22 +1,17 @@
-import Link from 'next/link';
-import NavBar from '../components/NavBar';
-import Header from './components/Header';
-import SearchSideBar from './components/SearchSideBar';
-import RestaurantCard from './components/RestaurantCard';
+import Header from "./components/Header";
+import SearchSideBar from "./components/SearchSideBar";
+import RestaurantCard from "./components/RestaurantCard";
 
 export default function Search() {
   return (
-    <div className="bg-gray-100 min-h-screen w-screen">
-      <div className="max-w-screen-2xl m-auto bg-white text-black">
-        <NavBar />
-        <Header />
-        <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-          <SearchSideBar />
-          <div className="w-5/6">
-            <RestaurantCard />
-          </div>
+    <>
+      <Header />
+      <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+        <SearchSideBar />
+        <div className="w-5/6">
+          <RestaurantCard />
         </div>
       </div>
-    </div>
+    </>
   );
 }
