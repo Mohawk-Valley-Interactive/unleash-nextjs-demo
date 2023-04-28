a6683a237c1e96a6cb3f496b1c4ecb7755361eca
Michael Vittiglio <michael.vittiglio@gmail.com>
2023-04-28 15:11:05 -0400

    17 Some more refactoring

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import NavBar from '../../components/NavBar';
+import Header from './components/Header';
+import Form from './components/Form';
 
 export default function Reserve() {
   return (
@@ -8,67 +10,8 @@ export default function Reserve() {
         <NavBar />
         <div className="border-t h-screen">
           <div className="w-3/5 py-9 m-auto">
-            {/* HEADER */}
-            <header>
-              <div className="font-bold">You're almost done!</div>
-              <div className="mt-5 flex">
-                <img
-                  src="https://resizer.otstatic.com/v2/photos/xlarge/2/39989965.jpg"
-                  alt=""
-                  className="w-32 h-18 rounded"
-                />
-                <div className="ml-4">
-                  <h1 className="font-bold text-3xl">Mike's Place</h1>
-                  <div className="flex mt-3">
-                    <p className="mr-6">Tues, 22nd 2023</p>
-                    <p className="mr-6">7:30 PM</p>
-                    <p className="mr-6">3 people</p>
-                  </div>
-                </div>
-              </div>
-            </header>
-            {/* HEADER */} {/* FORM */}
-            <div className="mt-10 flex flex-wrap justify-between w-[660px]">
-              <input
-                type="text"
-                className="bg-white border rounded p-3 w-80 mb-4"
-                placeholder="First name"
-              />
-              <input
-                type="text"
-                className="bg-white border rounded p-3 w-80 mb-4"
-                placeholder="Last name"
-              />
-              <input
-                type="text"
-                className="bg-white border rounded p-3 w-80 mb-4"
-                placeholder="Phone number"
-              />
-              <input
-                type="text"
-                className="bg-white border rounded p-3 w-80 mb-4"
-                placeholder="Email"
-              />
-              <input
-                type="text"
-                className="bg-white border rounded p-3 w-80 mb-4"
-                placeholder="Occasion (optional)"
-              />
-              <input
-                type="text"
-                className="bg-white border rounded p-3 w-80 mb-4"
-                placeholder="Requests (optional)"
-              />
-            </div>
-            <button className="bg-red-600 w-full p-3 text-white font-bold rounded disabled:bg-gray-300">
-              Complete reservation
-            </button>
-            <footer className="mt-4 text-sm">
-              By clicking "Complete reservation" you agree to the Terms of Use and Privacy Policy.
-              Standard text message rates may apply. You may opt out of receiving text messages at
-              any time.
-            </footer>
-            {/* FORM */}
+            <Header />
+            <Form />
           </div>
         </div>
       </div>
