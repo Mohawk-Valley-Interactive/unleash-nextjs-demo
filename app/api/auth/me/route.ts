a405c3aced36288e5af2db549dac3c9afeb86717
Michael Vittiglio <michael.vittiglio@gmail.com>
2023-05-07 11:02:08 -0400

    73 Authentication middleware created.

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,50 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
-import * as jose from "jose";
-import jwt from "jsonwebtoken";
 import prisma from "@/lib/prismaClient";
 
 export async function POST(req: NextRequest) {
-  const bearerToken = req.headers.get("authorization");
-  if (!bearerToken) {
-    return NextResponse.json(
-      { errorMessage: "Unauthorized request (no bearer token)." },
-      { status: 401 }
-    );
-  }
-
-  const token = bearerToken.split(" ")[1];
-  if (!token) {
-    return NextResponse.json(
-      { errorMessage: "Unauthorized request (bearer token empty)." },
-      { status: 401 }
-    );
-  }
-
-  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-  try {
-    await jose.jwtVerify(token, secret);
-  } catch (e) {
-    return NextResponse.json(
-      {
-        errorMessage: "Unauthorized request (bearer token cannot be verified).",
-      },
-      { status: 401 }
-    );
-  }
-
-  const tokenPayload = jwt.decode(token) as { email: string };
-
-  if (!tokenPayload.email) {
-    return NextResponse.json(
-      { errorMessage: "Unauthorized request (invalid email info)." },
-      { status: 401 }
-    );
-  }
+  // email acquired from token and injected via authentication middleware
+  const email: string = req.headers.get("email") as string;
 
   const user = await prisma.user.findUnique({
-    where: { email: tokenPayload.email },
+    where: { email },
     select: {
-      id: true,
       first_name: true,
       last_name: true,
       city: true,
@@ -52,6 +15,13 @@ export async function POST(req: NextRequest) {
       phone: true,
     },
   });
+
+  if (!user) {
+    return NextResponse.json(
+      { errorMessage: "Unauthorized request (user not found)." },
+      { status: 404 }
+    );
+  }
 
   return NextResponse.json({ user });
 }
