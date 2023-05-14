f299a6421ece55275c34eac4c92024596852d77b
Michael Vittiglio <michael.vittiglio@gmail.com>
2023-05-14 14:52:42 -0400

    114-116 Reservation page now using data.

--- app/reserve/[slug]/components/Form.tsx
+++ app/reserve/[slug]/components/Form.tsx
@@ -1,43 +1,98 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+
 export default function Form() {
+  const [disabled, setDisabled] = useState(true);
+  const [inputs, setInputs] = useState({
+    bookerFirstName: "",
+    bookerLastName: "",
+    bookerPhone: "",
+    bookerEmail: "",
+    bookerOccasion: "",
+    bookerRequest: "",
+  });
+
+  useEffect(() => {
+    if (
+      inputs.bookerFirstName &&
+      inputs.bookerLastName &&
+      inputs.bookerEmail &&
+      inputs.bookerPhone
+    ) {
+      return setDisabled(false);
+    }
+
+    setDisabled(true);
+  }, [inputs]);
+
+  function handleChangeInput(e: React.ChangeEvent<HTMLInputElement>) {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.currentTarget.value,
+    });
+  }
+
   return (
     <div className="mt-10 flex flex-wrap justify-between w-[660px]">
       <input
+        name="bookerFirstName"
         type="text"
         className="bg-white border rounded p-3 w-80 mb-4"
         placeholder="First name"
+        onChange={handleChangeInput}
+        value={inputs.bookerFirstName}
       />
       <input
+        name="bookerLastName"
         type="text"
         className="bg-white border rounded p-3 w-80 mb-4"
         placeholder="Last name"
+        onChange={handleChangeInput}
+        value={inputs.bookerLastName}
       />
       <input
+        name="bookerPhone"
         type="text"
         className="bg-white border rounded p-3 w-80 mb-4"
         placeholder="Phone number"
+        onChange={handleChangeInput}
+        value={inputs.bookerPhone}
       />
       <input
+        name="bookerEmail"
         type="text"
         className="bg-white border rounded p-3 w-80 mb-4"
         placeholder="Email"
+        onChange={handleChangeInput}
+        value={inputs.bookerEmail}
       />
       <input
+        name="bookerOccasion"
         type="text"
         className="bg-white border rounded p-3 w-80 mb-4"
         placeholder="Occasion (optional)"
+        onChange={handleChangeInput}
+        value={inputs.bookerOccasion}
       />
       <input
+        name="bookerRequest"
         type="text"
         className="bg-white border rounded p-3 w-80 mb-4"
         placeholder="Requests (optional)"
+        onChange={handleChangeInput}
+        value={inputs.bookerRequest}
       />
-      <button className="bg-red-600 w-full p-3 text-white font-bold rounded disabled:bg-gray-300">
+      <button
+        disabled={disabled}
+        className="bg-red-600 w-full p-3 text-white font-bold rounded disabled:bg-gray-300"
+      >
         Complete reservation
       </button>
       <footer className="mt-4 text-sm">
-        By clicking "Complete reservation" you agree to the Terms of Use and Privacy Policy.
-        Standard text message rates may apply. You may opt out of receiving text messages at any
-        time.
+        By clicking "Complete reservation" you agree to the Terms of Use and
+        Privacy Policy. Standard text message rates may apply. You may opt out
+        of receiving text messages at any time.
       </footer>
     </div>
   );
--- app/reserve/[slug]/components/Header.tsx
+++ app/reserve/[slug]/components/Header.tsx
@@ -1,19 +1,36 @@
-export default function Header() {
+import { Time, convertToDisplayTime } from "@/utils/convertToDisplayTime";
+import { format } from "date-fns";
+
+interface Props {
+  image: string;
+  name: string;
+  date: string;
+  partySize: string;
+}
+
+export default function Header({ image, name, date, partySize }: Props) {
+  partySize =
+    parseInt(partySize) == 1 ? `${partySize} person` : `${partySize} people`;
+
+  const [d, t] = date.split("T");
+  const displayTime = convertToDisplayTime(t as Time);
+  const displayDate = format(new Date(date), "ccc, LLL d");
+
   return (
     <header>
       <div className="font-bold">You're almost done!</div>
       <div className="mt-5 flex">
         <img
-          src="https://resizer.otstatic.com/v2/photos/xlarge/2/39989965.jpg"
+          src={image}
           alt=""
           className="w-32 h-18 rounded"
         />
         <div className="ml-4">
-          <h1 className="font-bold text-3xl">Mike's Place</h1>
+          <h1 className="font-bold text-3xl">{name}</h1>
           <div className="flex mt-3">
-            <p className="mr-6">Tues, 22nd 2023</p>
-            <p className="mr-6">7:30 PM</p>
-            <p className="mr-6">3 people</p>
+            <p className="mr-6">{displayDate}</p>
+            <p className="mr-6">{displayTime}</p>
+            <p className="mr-6">{partySize}</p>
           </div>
         </div>
       </div>
